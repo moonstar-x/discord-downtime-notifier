f027c277e102640a669e8294724164f8f82ad449
moonstar-x <christian.lopez99@outlook.com>
2018-11-19 17:36:36 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Discord Downtime Notifier Bot
+This is a small bot that notifies a server through a message when another bot goes offline. The main use is to let users know when a music bot goes offline and becomes unusable. It also lets users know once said bot goes back online. This is useful mostly for server owners with self-hosted bots and need a way of getting notified when they bots go offline. For better results, this bot should be hosted on a service like [Heroku](https://www.heroku.com/).
+
+## To Do:
+* This bot should work for multiple servers and multiple bots inside each server.
+* If possible, server ID shouldn't be required by the user.
+* It would also be interesting if the bot could also mention for how long has the specified bot been down.
+* I also need to write a little guide on how to use this bot on this readme.
---
+++ index.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./settings.json');
+
+let listenBot, messageChannel;
+
+client.on('ready', () => {
+  listenBot = client.guilds.get(config.server_id).members.get(config.listen_bot_id);
+  messageChannel = client.channels.get(config.notify_channel_id);
+
+  if (!listenBot.user.bot) {
+    throw new Error('Specified user is not a bot. Make sure the "listen_bot_id" under the settings.json file has the correct bot id.');
+  }
+
+  client.user.setPresence({
+    game: {
+      name: `${listenBot.nickname}`,
+      type: 'LISTENING'
+    }
+  });
+
+  console.log('Service started.');
+  console.log(`Listening to bot: ${listenBot.nickname}`);
+  console.log(`Will send messages to the ${messageChannel.name} channel.`);
+});
+
+client.on('presenceUpdate', (oldMember, newMember) => {
+  if (oldMember.id === config.listen_bot_id) {
+    let oldStatus = oldMember.presence.status;
+    let newStatus = newMember.presence.status;
+    if (oldStatus === 'online' && newStatus === 'offline') {
+      messageChannel.send(`The bot ${listenBot} has gone offline.`);
+    }
+    // Bot could change from 'offline' to 'dnd'.
+    if ((oldStatus !== 'online') && newStatus === 'online') {
+      messageChannel.send(`The bot ${listenBot} is now online.`);
+    }
+    console.log(`Status changed for ${listenBot.nickname} from ${oldStatus} to ${newStatus}!`);
+  }
+});
+
+client.login(config.discord_token);
