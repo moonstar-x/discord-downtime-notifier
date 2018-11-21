eec01e15c126cd1245e4490c74cb9d056a5dc228
moonstar-x <christian.lopez99@outlook.com>
2018-11-21 20:52:59 -0300

    Added how long the bot has been down to the message.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 const config = require('./settings.json');
 
 let listenBot, messageChannel;
+let timeSinceOffline = undefined;
 
 client.on('ready', () => {
   listenBot = client.guilds.get(process.env.SERVER_ID || config.server_id).members.get(process.env.BOT_ID || config.listen_bot_id);
@@ -29,14 +30,38 @@ client.on('presenceUpdate', (oldMember, newMember) => {
     let oldStatus = oldMember.presence.status;
     let newStatus = newMember.presence.status;
     if (oldStatus === 'online' && newStatus === 'offline') {
+      timeSinceOffline = Date.now();
       messageChannel.send(`The bot ${listenBot} has gone offline.`);
     }
     // Bot could change from 'offline' to 'dnd'.
     if ((oldStatus !== 'online') && newStatus === 'online') {
-      messageChannel.send(`The bot ${listenBot} is now online.`);
+      if (timeSinceOffline) {
+        let offlineTime = Date.now() - timeSinceOffline;
+        messageChannel.send(`The bot ${listenBot} is now online. It has been offline for ${displayTime(offlineTime)}.`);
+      } else {
+        messageChannel.send(`The bot ${listenBot} is now online.`);
+      } 
     }
     console.log(`Status changed for ${listenBot.nickname} from ${oldStatus} to ${newStatus}!`);
   }
 });
 
 client.login(process.env.DISCORD_TOKEN || config.discord_token);
+
+function displayTime(millis) {
+  const totalTime = Math.floor(millis / 1000);
+  const days = Math.floor(totalTime / 86400);
+  const hours = Math.floor(totalTime / 3600) % 24;
+  const minutes = Math.floor(totalTime / 60) % 60;
+  const seconds = totalTime % 60;
+
+  let result = [];
+  const timeArr = [days, 'days', hours, 'hours', minutes, 'minutes', seconds, 'seconds'];
+  for (let i = 0; i < timeArr.length; i+=2) {
+    if (timeArr[i] !== 0) {
+      result.push(timeArr[i], timeArr[i + 1]);
+    }
+  }
+  return result
+    .join(' ');
+}
