dd33b524e51caffc40825c47017477658378c40e
moonstar-x <christian.lopez99@outlook.com>
2019-09-20 20:03:35 -0500

    Refactored main files to use mongo.

--- src/app.js
+++ src/app.js
@@ -3,13 +3,13 @@ const fs = require('fs-extra');
 const path = require('path');
 const appEvents = require('./events/appEvents');
 const appHandlers = require('./events/handlers/app');
-const Realm = require('./classes/Realm');
+const Mongo = require('./classes/Mongo');
 
 const token = process.env.DISCORD_TOKEN || require('../config/settings.json').discord_token;
 
 const client = new Client();
 client.commands = new Collection();
-const realm = new Realm(client);
+const mongo = new Mongo(client);
 
 const commandFiles = fs.readdirSync(path.join(__dirname, '/commands')).filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
@@ -19,13 +19,13 @@ for (const file of commandFiles) {
 
 client.on(appEvents.disconnect, (event) => appHandlers.handleDisconnect(event));
 client.on(appEvents.error, (error) => appHandlers.handleError(error));
-client.on(appEvents.guildCreate, (guild) => appHandlers.handleGuildCreate(guild, realm));
-client.on(appEvents.guildDelete, (guild) => appHandlers.handleGuildDelete(guild, realm));
+client.on(appEvents.guildCreate, (guild) => appHandlers.handleGuildCreate(guild, mongo));
+client.on(appEvents.guildDelete, (guild) => appHandlers.handleGuildDelete(guild, mongo));
 client.on(appEvents.guildUnavailable, (guild) => appHandlers.handleGuildUnavailable(guild));
-client.on(appEvents.guildMemberRemove, (member) => appHandlers.handleMemberDelete(member, realm));
-client.on(appEvents.message, (message) => appHandlers.handleMessage(message, realm));
-client.on(appEvents.presenceUpdate, (oldMember, newMember) => appHandlers.handlePresenceUpdate(oldMember, newMember, realm));
-client.on(appEvents.ready, () => appHandlers.handleReady(realm));
+client.on(appEvents.guildMemberRemove, (member) => appHandlers.handleMemberDelete(member, mongo));
+client.on(appEvents.message, (message) => appHandlers.handleMessage(message, mongo));
+client.on(appEvents.presenceUpdate, (oldMember, newMember) => appHandlers.handlePresenceUpdate(oldMember, newMember, mongo));
+client.on(appEvents.ready, () => appHandlers.handleReady(mongo));
 client.on(appEvents.reconnecting, () => appHandlers.handleReconnecting());
 client.on(appEvents.resume, () => appHandlers.handleResume());
 client.on(appEvents.warn, (info) => appHandlers.handleReady(info));
